d23d775a53ccdc297fa7e7de7bfe7e0b2978ce30
蔡凱翔 <eeit10506@gmail.com>
2019-03-13 18:37:00 +0800

    aa

--- RentHouse/src/main/java/com/iiiedu105/RentHouse/model/Forum.java
+++ RentHouse/src/main/java/com/iiiedu105/RentHouse/model/Forum.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
