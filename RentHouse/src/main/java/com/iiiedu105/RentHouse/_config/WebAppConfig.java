0318b8b63fe6e997a2141cd7de0806d346ef5c70
蔡凱翔 <eeit10506@gmail.com>
2019-03-13 18:32:22 +0800

    111

--- RentHouse/src/main/java/com/iiiedu105/RentHouse/_config/WebAppConfig.java
+++ RentHouse/src/main/java/com/iiiedu105/RentHouse/_config/WebAppConfig.java
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("com.iiiedu105.RentHouse.model")
+@ComponentScan("com.iiiedu105.RentHouse")
 public class WebAppConfig extends WebMvcConfigurerAdapter {
 
 
@@ -25,5 +26,15 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
 		resolver.setPrefix("/WEB-INF/views/");//EX: /WEB-INF/views/ + ??????
 		resolver.setSuffix(".jsp");//EX: ???? + .jsp
 		return resolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**")
+				.addResourceLocations("/WEB-INF/views/css/");
+		registry.addResourceHandler("/images/**")
+				.addResourceLocations("/WEB-INF/views/images/");
+//		registry.addResourceHandler("/js/**")
+//				.addResourceLocations("/WEB-INF/views/js/jQuery-TWzipcode-master/");
 	}
 }
