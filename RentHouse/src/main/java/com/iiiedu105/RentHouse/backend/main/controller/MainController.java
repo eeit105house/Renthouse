59b9ed04f40e6888434f6c8331c469b38ef50253
吳友廷 <0900075@gmail.com>
2019-03-13 17:04:39 +0800

    後台重新傳

---
+++ RentHouse/src/main/java/com/iiiedu105/RentHouse/backend/main/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.iiiedu105.RentHouse.backend.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+//	@RequestMapping(value="/")
+//	public String bsindex() {
+//		return "bsindex";		
+//	}
+	@RequestMapping(value="/bslogout")
+	public String login() {
+		return "bslogout";		
+	}
+
+	@RequestMapping(value="/chart")
+	public String chart() {
+		return "chart";		
+	}
+	@RequestMapping(value="/customerservice")
+	public String customerservice() {
+		return "customerservice";		
+	}
+}
